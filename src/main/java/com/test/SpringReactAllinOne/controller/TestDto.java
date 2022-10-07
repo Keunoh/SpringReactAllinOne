f802656f119a3b663da6305592a2223f65630b49
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-07 15:05:13 +0900

    error page when customController invoke

--- src/main/java/com/test/SpringReactAllinOne/controller/TestDto.java
+++
@@ -1,9 +0,0 @@
-package com.test.SpringReactAllinOne.controller;
-
-import lombok.Getter;
-
-@Getter
-public class TestDto {
-    private String hello = "hello !";
-    private String message = "cors ignore";
-}
