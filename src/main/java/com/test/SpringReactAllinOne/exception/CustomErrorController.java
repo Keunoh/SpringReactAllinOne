341897b982d5302db2117af4d0975ca6688558bb
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-08 23:14:05 +0900

    delete comment

--- src/main/java/com/test/SpringReactAllinOne/exception/CustomErrorController.java
+++ src/main/java/com/test/SpringReactAllinOne/exception/CustomErrorController.java
@@ -3,23 +3,12 @@ package com.test.SpringReactAllinOne.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
 public class CustomErrorController implements ErrorController {
-
-//    @RequestMapping(value = "/error")
-//    @ResponseBody
-//    public ErrorMessageDto errorMessage(HttpServletRequest request) {
-//        Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-//        Object exceptionType = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
-//        Object exceptionMessage = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
-//        return new ErrorMessageDto(statusCode, exceptionType, exceptionMessage);
-//    }
 
     @RequestMapping("/errorTest")
     public String errorTest(){
