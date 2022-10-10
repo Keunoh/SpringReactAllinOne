74e220c8ebb297f2fb2e46bf0211358dcdd88eb7
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-10 22:21:43 +0900

    setting configure

---
+++ src/main/java/com/test/SpringReactAllinOne/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.test.SpringReactAllinOne.config;
+
+import com.test.SpringReactAllinOne.security.JwtAuthenticationEntryPoint;
+import com.test.SpringReactAllinOne.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //보안 resource에 인증되지 않은 접근 발생시 401 에러 return
+    //spring security의 AuthenticationEntryPoint interface를 implements
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Bean
+    public JwtAuthenticationFilter JwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        //Add out custom JWT security filter
+        http.addFilterBefore(JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+}
