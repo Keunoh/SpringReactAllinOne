04842fdd8d26f661e632af2eabb9e9167f5ac9db
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-11 17:49:58 +0900

    add method 1

--- src/main/java/com/test/SpringReactAllinOne/repository/UserRepository.java
+++ src/main/java/com/test/SpringReactAllinOne/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.test.SpringReactAllinOne.repository;
 
-import com.test.SpringReactAllinOne.domain.Member;
+import com.test.SpringReactAllinOne.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByMemberIdAndMemberPw(String memberId, String memberPw);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserIdAndUserPw(String userId, String userPw);
+    Optional<User> findByUserId(String userId);
 }
