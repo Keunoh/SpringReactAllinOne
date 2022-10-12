596227aaf556126e8ae6f1d102a493e8cdb9d855
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-12 16:16:43 +0900

    simple request

---
+++ src/main/java/com/test/SpringReactAllinOne/dto/SignUpRequestDto.java
@@ -0,0 +1,11 @@
+package com.test.SpringReactAllinOne.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpRequestDto {
+    private String username;
+    private String password;
+}
