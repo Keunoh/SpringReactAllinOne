cd0425c6a71687466b7723488c28600208025f6d
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-12 16:12:20 +0900

    delete unused methods

--- src/main/java/com/test/SpringReactAllinOne/repository/UserRepository.java
+++ src/main/java/com/test/SpringReactAllinOne/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserIdAndUserPw(String userId, String userPw);
-    Optional<User> findByUserId(String userId);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
 }
