09c3a09a74f5d85799fd65139b0ff2ba6b5a3566
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-12 16:07:08 +0900

    AuthenticationManager and PasswordEncoder update

--- src/main/java/com/test/SpringReactAllinOne/config/SecurityConfig.java
+++ src/main/java/com/test/SpringReactAllinOne/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.test.SpringReactAllinOne.config;
 
+import com.test.SpringReactAllinOne.security.CustomUserDetailsService;
 import com.test.SpringReactAllinOne.security.JwtAuthenticationEntryPoint;
 import com.test.SpringReactAllinOne.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -19,6 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //spring security의 AuthenticationEntryPoint interface를 implements
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     /**
      * 이 부분이 만약 Component로 주입되었을 때 오류가 발생하는지 알아보기로 한다.
@@ -54,10 +62,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js")
                 .permitAll()
+                .antMatchers("/api/v1/user/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
 
         //Add out custom JWT security filter
         http.addFilterBefore(JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
