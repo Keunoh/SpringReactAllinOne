6dad19b334d2eef26c10d8d40759cafc4515212d
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-13 15:32:29 +0900

    delete unused member variables

--- src/main/java/com/test/SpringReactAllinOne/domain/User.java
+++ src/main/java/com/test/SpringReactAllinOne/domain/User.java
@@ -16,11 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
     private String username;
-
-    private String email;
 
     private String password;
 
