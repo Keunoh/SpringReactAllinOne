942c7b119ca415dd2432d111c0e7470751652213
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-13 15:36:55 +0900

    update method

--- src/main/java/com/test/SpringReactAllinOne/service/UserService.java
+++ src/main/java/com/test/SpringReactAllinOne/service/UserService.java
@@ -1,8 +1,6 @@
 package com.test.SpringReactAllinOne.service;
 
 import com.test.SpringReactAllinOne.domain.User;
-import com.test.SpringReactAllinOne.dto.LoginRequestDto;
-import com.test.SpringReactAllinOne.dto.LoginResponseDto;
 import com.test.SpringReactAllinOne.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,14 +11,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public LoginResponseDto getUserIdAndPassword(LoginRequestDto loginRequestDto){
-
-        String userId = loginRequestDto.getUsernameOrEmail();
-        String userPw = loginRequestDto.getPassword();
-
-        User userData = userRepository.findByUsernameOrEmail(userId, userPw)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
-
-        return new LoginResponseDto(userData);
+    public void simpleCreateUser(User user){
+        userRepository.save(user);
     }
 }
