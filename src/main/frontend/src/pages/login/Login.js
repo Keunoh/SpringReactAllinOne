7fa45e63da66c28ec2e6273a3af14a1d2daced83
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-16 20:22:24 +0900

    signUp area update

--- src/main/frontend/src/pages/login/Login.js
+++ src/main/frontend/src/pages/login/Login.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import { signin } from "utils/APIUtils";
+import { signin, signup } from "utils/APIUtils";
 
 const Login = () => {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const [signUpUsername, setSignUpUsername] = useState('');
+    const [signUpPassword, setSignUpPassword] = useState('');
+
+    const loginHandleSubmit = (e) => {
         //e.preventDefault() 적어주지 않으면
         //계속해서 새로고침이 된다.
         e.preventDefault();
@@ -21,20 +24,43 @@ const Login = () => {
 
         signin(loginRequestDto)
         .then(response => {
-            console.log("login success");
+            console.log("SignIn SUCCESS");
             console.log(response);
         })
         .catch(error => {
-            console.log(error);
+            console.log("SignIn FAILED");
+            console.log("SignIn Error Message : " + error);
         });
 
     }
 
+    const signUpHandleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Click Sign Up Button");
+        console.log("SignUp user ID : " + signUpUsername);
+        console.log("SignUp user PW : " + signUpPassword);
+
+        let signUpRequestDto = {
+            username: signUpUsername,
+            password: signUpPassword
+        }
+
+        signup(signUpRequestDto)
+        .then(response => {
+            console.log("SignUp SUCCESS");
+            console.log(response);
+        })
+        .catch(error => {
+            console.log("SignUp FAILED");
+            console.log("SignUp Error Message : " + error);
+        })
+    }
+
     return (
       <div>
-        <h1>Login Page</h1>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="userId">
+        <h1>Login Area</h1>
+        <form onSubmit={loginHandleSubmit}>
+            <label htmlFor="username">
                 ID :
             </label>
             <input 
@@ -58,6 +84,36 @@ const Login = () => {
             <button>
                 Submit
             </button> 
+        </form>
+
+        <h2>----</h2>
+
+        <h1>SignUp Area</h1>
+        <form onSubmit={signUpHandleSubmit}>
+            <label htmlFor="signUpUsername">
+                ID : 
+            </label>
+            <input 
+                type="text"
+                name="signUpUsername"
+                value={signUpUsername}
+                autoComplete="off"
+                onChange={(e) => setSignUpUsername(e.target.value)}
+                required
+            />
+            <label htmlFor="signUpPassword">
+                PW : 
+            </label>
+            <input 
+                type="password"
+                name="signUpPassword"
+                value={signUpPassword}
+                onChange={(e) => setSignUpPassword(e.target.value)}
+                required
+            />
+            <button>
+                Sign Up
+            </button>
         </form>
       </div>
     );
