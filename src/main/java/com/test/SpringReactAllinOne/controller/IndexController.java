8dce6e8cb939106c28ac042c60fc8f6209c8cf69
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-17 21:29:28 +0900

    return MAV version or String version

--- src/main/java/com/test/SpringReactAllinOne/controller/IndexController.java
+++ src/main/java/com/test/SpringReactAllinOne/controller/IndexController.java
@@ -2,15 +2,21 @@ package com.test.SpringReactAllinOne.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
+//    @GetMapping("")
+//    public ModelAndView home(){
+//        ModelAndView mav = new ModelAndView("index");
+//        return mav;
+//    }
+//
     @GetMapping("")
-    public ModelAndView home(){
-        ModelAndView mav = new ModelAndView("index");
-        return mav;
+    public String home2(){
+        return "index";
     }
 
 }
