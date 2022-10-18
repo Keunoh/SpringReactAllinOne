d6ed840a207ddb4aec7df20eecd169d0b6dbec27
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-18 15:45:32 +0900

    index method

--- src/main/java/com/test/SpringReactAllinOne/controller/IndexController.java
+++ src/main/java/com/test/SpringReactAllinOne/controller/IndexController.java
@@ -1,21 +1,13 @@
 package com.test.SpringReactAllinOne.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-//    @GetMapping("")
-//    public ModelAndView home(){
-//        ModelAndView mav = new ModelAndView("index");
-//        return mav;
-//    }
-//
-    @GetMapping("")
-    public String home2(){
+    @RequestMapping("")
+    public String index(){
         return "index";
     }
 
